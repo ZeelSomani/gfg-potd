eadd5217ed0f1d3b432a24936f2d618d53a75f28
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-05-25 18:07:28 +0530

    25-05-2024.cpp

---
+++ 25-05-2024.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+  public:
+    long long max_Books(int arr[], int n, int k) 
+    {
+        long long ans=0, curr=0;
+        int i=0;
+        while(i<n)
+        {
+            if(arr[i]<=k)
+            {
+                curr+=arr[i];
+            }
+            else
+            {
+                curr=0;
+            }
+            
+            ans=max(ans, curr);
+            i++;
+        }
+        return ans;
+    }
+};
