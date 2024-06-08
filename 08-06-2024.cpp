da67a3529e4566c04c20551d74495a8c7441e0c0
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-06-08 18:09:02 +0530

    08-06-2024.cpp

---
+++ 08-06-2024.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+  public:
+  int findExtra(int n, int arr1[], int arr2[]) 
+  {
+        for(int i=0; i<n; i++)
+        {
+            if(arr1[i]!=arr2[i])
+            {
+                return i;
+            }
+        }
+    }
+};
