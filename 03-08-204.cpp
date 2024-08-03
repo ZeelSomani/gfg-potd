45bc1cb80279459a0f20a9f33a04364dd8989e86
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-08-03 11:45:56 +0530

    03-08-204.cpp

---
+++ 03-08-204.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+  public:
+    int celebrity(vector<vector<int> >& mat) 
+    {
+        int n=mat.size(),m=mat[0].size();
+        for(int i=0;i<n;i++)
+        {
+            int row=0,col=0;
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0) row++;
+                if(mat[j][i]==1) col++;
+            }
+            if(row==n && col==n-1) return i;
+        }
+        return -1;
+    }
+};
