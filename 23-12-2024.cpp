69bb363643f1518f150f423f8dc143355efae0f2
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-23 22:16:58 +0530

    23-12-2024.cpp

---
+++ 23-12-2024.cpp
@@ -0,0 +1,12 @@
+class Solution 
+{
+public:
+    bool searchRowMatrix(vector<vector<int>>& mat, int x) 
+      {
+        for (auto& row : mat)
+        {
+            if (binary_search(row.begin(), row.end(), x)) return true;
+        }
+        return false;
+    }
+};
