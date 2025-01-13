d8e485f528798592e01529df169c11c48c3d70c1
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2025-01-13 09:12:26 +0530

    13-01-2025.cpp

---
+++ 13-01-2025.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+  public:
+    int maxWater(vector<int> &arr) 
+     {
+         int n = arr.size();
+    int left = 0, right = n - 1; 
+    int maxArea = 0;
+    while (left < right) 
+     {
+        int height = min(arr[left], arr[right]);
+        int width = right - left;
+        int area = height * width;
+        maxArea = max(maxArea, area);
+        if (arr[left] < arr[right]) 
+        {
+            left++;
+        } 
+         else 
+        {
+            right--;
+        }
+    }
+    return maxArea;
+    }
+};
