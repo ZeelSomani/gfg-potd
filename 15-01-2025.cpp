11b75eaa34c75f888dc52213e72a588cc9f746c3
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2025-01-15 09:47:23 +0530

    15-01-2025.cpp

---
+++ 15-01-2025.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+  public:
+    int longestSubarray(vector<int>& arr, int k) 
+    {
+        unordered_map<int, int> prefixSumMap;
+      int maxLength = 0;
+      int currentSum = 0;
+    for (int i = 0; i < arr.size(); i++)
+      {
+        currentSum += arr[i];
+        if (currentSum == k)
+        {
+            maxLength = i + 1;
+        }
+        if (prefixSumMap.find(currentSum - k) != prefixSumMap.end())
+        {
+            maxLength = max(maxLength, i - prefixSumMap[currentSum - k]);
+        }
+        if (prefixSumMap.find(currentSum) == prefixSumMap.end()) 
+        {
+            prefixSumMap[currentSum] = i;
+        }
+    }
+    return maxLength;
+    }
+};
